6889c62b44ec2b8f6e7ab322a95231b67d125df3
KRITHIKA <sec25co010@sairamtap.edu.in>
2026-02-04 11:02:47 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
-export default function
-handler (req,res) {
-  res.status(200).json({ message: "API working!"});
+export default function handler(req, res) {
+  const apiKey = req.headers["x-api-key"];
+
+  if (apiKey !== process.env.API_KEY) {
+    return res.status(401).json({ error: "Invalid API key" });
+  }
+
+  res.status(200).json({ message: "API working!" });
 }
